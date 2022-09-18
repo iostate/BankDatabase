75417f6742bd5be1dc60217fb00a7cc33dd41e6e
iostate <qmtruong92@gmail.com>
2022-09-18 13:51:59 -0700

    Add SQLC code for CRUD entries

---
+++ db/query/entries.sql
@@ -0,0 +1,27 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries 
+WHERE id = $1 LIMIT 1;
+
+-- name: GetEntryForUpdate :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+WHERE id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE id = $1;
